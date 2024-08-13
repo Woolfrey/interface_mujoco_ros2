c959e68c5736f5ecd5c67c97a567ccdb8651870a
Jon <jonathan.woolfrey@gmail.com>
2024-08-13 16:59:29 +0100

    Fixed to match changes to class file

--- src/mujoco_interface_node.cpp
+++ src/mujoco_interface_node.cpp
@@ -1,5 +1,5 @@
 /**
- * @file   mujoco_interface_node.cpp
+ * @file   mujocoSim_node.cpp
  * @author Jon Woolfrey
  * @data   August 2024
  * @brief  Creates a ROS2 node that interfaces with a MuJoCo simulation.
@@ -10,47 +10,59 @@
 
 int main(int argc, char *argv[])
 {
-    rclcpp::init(argc, argv);
+    rclcpp::init(argc, argv);                                                                       // Starts up ROS2
+    
+    auto node = std::make_shared<rclcpp::Node>("mujocoSim_parameters");                             // Create a node to access parameters
 
-    // Create a node to access parameters
-    auto node = std::make_shared<rclcpp::Node>("mujoco_interface_parameters");
-
-    // Load parameters from the parameter server
-    std::string xml_location = node->declare_parameter<std::string>("xml_location", "");
-    std::string control_mode_str = node->declare_parameter<std::string>("control_mode", "TORQUE");
-    std::string publisher_name = node->declare_parameter<std::string>("publisher_name", "joint_states");
-    std::string subscriber_name = node->declare_parameter<std::string>("subscriber_name", "joint_controls");
+    // Load simulation parameters from the server
+    int simulationFrequency    = node->declare_parameter<int>("simulation_frequency", 1);
+    int visualizationFrequency = node->declare_parameter<int>("visualization_frequency", 20);
+    std::string xmlLocation    = node->declare_parameter<std::string>("xml_location", "");
+    std::string controlMode    = node->declare_parameter<std::string>("control_mode", "TORQUE");
+    std::string publisherName  = node->declare_parameter<std::string>("publisher_name", "joint_states");
+    std::string subscriberName = node->declare_parameter<std::string>("subscriber_name", "joint_commands");
 
     // Set the control mode
     ControlMode control_mode;
-         if (control_mode_str == "POSITION") control_mode = POSITION;
-    else if (control_mode_str == "VELOCITY") control_mode = VELOCITY;
-    else if (control_mode_str == "TORQUE")   control_mode = TORQUE;
-    else                                     control_mode = UNKNOWN;
+         if (controlMode == "POSITION") control_mode = POSITION;
+    else if (controlMode == "VELOCITY") control_mode = VELOCITY;
+    else if (controlMode == "TORQUE")   control_mode = TORQUE;
+    else                                control_mode = UNKNOWN;
 
-    // Create MuJoCoInterface object
-    auto mujoco_interface = std::make_shared<MuJoCoInterface>(xml_location, publisher_name, subscriber_name, control_mode);
+    // Create MuJoCo simulation
+    auto mujocoSim = std::make_shared<MuJoCoInterface>(
+        xmlLocation,
+        publisherName,
+        subscriberName,
+        control_mode,
+        simulationFrequency,
+        visualizationFrequency);
 
     // Load and set feedback gains
-    double proportional_gain = node->declare_parameter<double>("proportional_gain", 0.0);
-    double derivative_gain = node->declare_parameter<double>("derivative_gain", 0.0);
-    double integral_gain = node->declare_parameter<double>("integral_gain", 0.0);
+    double proportionalGain = node->declare_parameter<double>("proportional_gain", 1.0);
+    double derivativeGain   = node->declare_parameter<double>("derivative_gain", 0.01);
+    double integralGain     = node->declare_parameter<double>("integral_gain", 0.0);
 
-    mujoco_interface->set_feedback_gains(proportional_gain, integral_gain, derivative_gain);
+    mujocoSim->set_feedback_gains(proportionalGain, integralGain, derivativeGain);
 
     // Load and set camera properties
     std::vector<double> camera_focal_point = node->declare_parameter<std::vector<double>>("camera_focal_point", {0.0, 0.0, 0.0});
-    double camera_distance = node->declare_parameter<double>("camera_distance", 1.0);
-    double camera_azimuth = node->declare_parameter<double>("camera_azimuth", 0.0);
-    double camera_elevation = node->declare_parameter<double>("camera_elevation", 0.0);
-    bool camera_orthographic = node->declare_parameter<bool>("camera_orthographic", false);
+    double camera_distance                 = node->declare_parameter<double>("camera_distance", 1.0);
+    double camera_azimuth                  = node->declare_parameter<double>("camera_azimuth", 0.0);
+    double camera_elevation                = node->declare_parameter<double>("camera_elevation", 0.0);
+    bool camera_orthographic               = node->declare_parameter<bool>("camera_orthographic", false);
 
-    mujoco_interface->set_camera_properties({camera_focal_point[0], camera_focal_point[1], camera_focal_point[2]}, camera_distance, camera_azimuth, camera_elevation, camera_orthographic);
+    mujocoSim->set_camera_properties(
+        {camera_focal_point[0], camera_focal_point[1], camera_focal_point[2]},
+        camera_distance,
+        camera_azimuth, 
+        camera_elevation,
+        camera_orthographic);
 
-    // Spin the node
-    rclcpp::spin(mujoco_interface);
+    rclcpp::spin(mujocoSim);                                                                        // Run the simulation indefinitely
 
-    rclcpp::shutdown();
+    rclcpp::shutdown();                                                                             // Shut down
+    
     return 0;
 }
 
