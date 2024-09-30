36f6af78cce3e4f64ddb2fce21e22ab0bb771185
Jon <jonathan.woolfrey@gmail.com>
2024-09-30 15:27:29 +0100

    Removed obsolete functions for setting joint gains

--- src/mujoco_interface_node.cpp
+++ src/mujoco_interface_node.cpp
@@ -16,16 +16,12 @@ int main(int argc, char *argv[])
     
     auto node = std::make_shared<rclcpp::Node>("mujoco_sim_parameters");                            // Create a node to access parameters
 
-    int simulationFrequency    = node->declare_parameter<int>("simulation_frequency", 1);
+    int simulationFrequency    = node->declare_parameter<int>("simulation_frequency", 500);
     int visualizationFrequency = node->declare_parameter<int>("visualization_frequency", 20);
     std::string xmlLocation    = node->declare_parameter<std::string>("xml_path", "");
     std::string controlMode    = node->declare_parameter<std::string>("control_mode", "TORQUE");
     std::string publisherName  = node->declare_parameter<std::string>("publisher_name", "joint_states");
     std::string subscriberName = node->declare_parameter<std::string>("subscriber_name", "joint_commands");
-
-    double proportionalGain = node->declare_parameter<double>("proportional_gain", 1.0);
-    double derivativeGain   = node->declare_parameter<double>("derivative_gain", 0.01);
-    double integralGain     = node->declare_parameter<double>("integral_gain", 0.0);
 
     // Load camera parameters
     std::vector<double> camera_focal_point = node->declare_parameter<std::vector<double>>("camera_focal_point", {0.0, 0.0, 0.0});
@@ -51,8 +47,6 @@ int main(int argc, char *argv[])
                                                            control_mode,
                                                            simulationFrequency,
                                                            visualizationFrequency);
-
-        mujocoSim->set_feedback_gains(proportionalGain, integralGain, derivativeGain);
 
         mujocoSim->set_camera_properties({camera_focal_point[0], camera_focal_point[1], camera_focal_point[2]},
                                           camera_distance,
